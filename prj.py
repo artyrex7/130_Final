2bb2ccf54c196202f23c11f0a492d070b3d11343
Arturo <32753509+artyrex7@users.noreply.github.com>
2018-12-10 17:36:09 -0800

    Add files via upload

---
+++ prj.py
@@ -0,0 +1,23 @@
+import subprocess
+
+users = dict()
+with open('users','r') as file:
+    for i in file.readlines():
+        users[i.split()[0]] = i.split()[1]
+        
+cont = 'y'
+while cont == 'y':
+    user = raw_input('Enter Username: ')
+    paswd = raw_input('Enter Password: ')
+    out = subprocess.Popen("./sha256.exe",stdin=subprocess.PIPE,stdout=subprocess.PIPE).communicate(paswd)[0]
+    if user in users:
+        if users[user] == out:
+            print('Username and Password Correct!\n')
+            
+        else:
+            print('Username or Password Incorrect\n')
+    else:
+        print('UserName or Password Incorrect\n')
+
+    cont = raw_input('Continue?(y/n) ')
+    
